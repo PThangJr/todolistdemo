fe3ac070944b121ea8d992f2d4d656a699c892a8
Phạm Thắng <pthjrr@gmail.com>
2020-10-16 14:34:24 +0700

    Updates

---
+++ precache-manifest.5b3e2bd0dc110e788cf2a5534868f444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5bd02a431f2b8bfa60349ab4136ea2",
+    "url": "/todolistdemo/index.html"
+  },
+  {
+    "revision": "fc9914df2cb7e10857d7",
+    "url": "/todolistdemo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bb0484e9b2f2da439d43",
+    "url": "/todolistdemo/static/js/2.0f352530.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolistdemo/static/js/2.0f352530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc9914df2cb7e10857d7",
+    "url": "/todolistdemo/static/js/main.a4e51a34.chunk.js"
+  },
+  {
+    "revision": "245e5cf6ea1700ae96a4",
+    "url": "/todolistdemo/static/js/runtime-main.e0accce5.js"
+  }
+]);
